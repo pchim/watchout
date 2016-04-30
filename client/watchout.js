011c5b20681ab7a8c62a3b9c95a9b6c5e751a73f
pchim <pchim24@gmail.com>
2016-04-30 14:50:53 -0700

    change background and display level

--- client/watchout.js
+++ client/watchout.js
@@ -3,40 +3,74 @@
 var currentScore = 0;
 var highScore = 0;
 var collisions = 0;
+var level = 0;
 
-var svg = d3.select('svg'); 
+var svg = d3.select('body').selectAll('svg'); 
 svg.append('image')
-   .attr('xlink:href', 'assets/desert_map.png')
+   .attr('xlink:href', 'assets/city4.png')
    .attr('width', '700')
    .attr('height', '700');
 var refract = false;
-/*
 
-xlink:href="assets/link_norm.gif" x="0" y="0" height="50" width="50"
-
-*/
 var step = function() {
-  // store user object - css class user
-
-  // d3 select all enemies - enemy class
-  // check if enemy cx, cy + radius
-  // if within user object cx, cy + radius
+ 
   var enemy = d3.selectAll('image.enemy');
   enemy.transition() .duration(1000)
         .attr('x', function() { return Math.random() * 600; })
-        //.attr('src', function() { return 'asteroid.png'; })
-        .attr('y', function() { return Math.random() * 600 });
+        .attr('y', function() { return Math.random() * 600; });
 
   currentScore++;
   d3.select('.current span')
     .text(currentScore);
   
+  var level = 0;
+  if (currentScore === 10) {
+    level = 1 ;
+    var levelBoard = d3.select('svg')
+      .selectAll('g')
+      .data([1])
+      .enter()
+      .append('g');
+ 
+
+    levelBoard.append('rect')
+      .attr ('x', '270')
+      .attr ('y', '670')
+      .attr ('rx', '10')
+      .attr ('ry', '10')
+      .attr ('height', '30')
+      .attr ('width', '200')
+      .attr ('fill', 'red');
+
+
+    levelBoard.append('text')
+              .text (function(d) { 
+                var txt = 'You are in level - ' + level;
+                return txt;
+              })
+              .attr('font-size', '20px')
+              .attr('fill', 'blue')
+              .attr ('x', '300')
+              .attr ('y', '690')
+              .attr ('align', 'center');
+
+
+    makeEnemies();
+
+
+  }
+
+  // if (levelUp) {
+  //   levelUp = false;
+  //   levelBoard.remove();
+   
+  // }
   setTimeout(step, 1000);  
 };
 
 
 
-var dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var dataset = [1, 2, 3, 4, 5];
 d3.select('svg')
   .selectAll('image.enemy')
   .data(dataset)
@@ -62,25 +96,41 @@ var enemy = d3.selectAll('image.enemy');
 
 var user = d3.select('image.user');
 var userH = user.attr('height') / 2;
-var userW = user.attr('width') / 2 ;
+var userW = user.attr('width') / 2;
 var enemyH = enemy.attr('height') / 2;
-var enemyW = enemy.attr('width') / 2 ;
+var enemyW = enemy.attr('width') / 2;
 
 var drag = d3.behavior.drag()
-  //.on ('dragstart', function() { user.style('fill', 'red'); })
-  .on ('drag', function() { user.attr('x', d3.event.x - userW)
-                                .attr('y', d3.event.y - userH) });
-  //.on('dragend', function() { user.style('fill', 'black'); });
+  .on ('drag', function() { 
+    user.attr('x', d3.event.x - userW)
+        .attr('y', d3.event.y - userH); 
+
+    if (user.attr('x') > 650) {
+      user.attr('x', '650');
+    }
+    if (user.attr('y') > 650) {
+      user.attr('y', '650'); 
+    }
+    if (user.attr('x') < 0) {
+      user.attr('x', '0');
+    }
+    if (user.attr('y') < 0) {
+      user.attr('y', '0'); 
+    }
+
+
+  });
+
+
 
 var userStep = function() {
 
   var enemies = d3.selectAll('image.enemy');
   enemies.each( function(enemy) {
+
     var xDist = (Number(user.attr('x')) + userW) - (Number(d3.select(this).attr('x')) + enemyW);
     var yDist = (Number(user.attr('y')) + userH) - (Number(d3.select(this).attr('y')) + enemyH);
     var maxDist = userW + enemyW;
-
-    console.log(maxDist);
 
     var currDist = Math.sqrt( Math.pow(xDist, 2) + Math.pow(yDist, 2));
     
@@ -105,7 +155,7 @@ var userStep = function() {
 var refractory = function() {
   refract = true;
   user.classed('hit', true);
-  setTimeout(unrefractory, 2000);
+  setTimeout(unrefractory, 1200);
 };
 
 var unrefractory = function() {
@@ -113,9 +163,23 @@ var unrefractory = function() {
   user.classed('hit', false);
 };
 
+var makeEnemies = function() {
+  var makeEnemyData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  d3.select('svg')
+    .selectAll('image.enemy')
+    .data(makeEnemyData)
+    .enter()
+    .append('image')
+    .attr('xlink:href', 'assets/leever_2.gif')
+       .attr('x', '0')
+       .attr('y', '0')
+       .attr('height', '50')
+       .attr('width', '50')
+       .classed('enemy', true);
+};
+
+
 step();
 userStep();
 user.call(drag);
 
-// player or enemies should not move out of play area
-// start player in the corner 
